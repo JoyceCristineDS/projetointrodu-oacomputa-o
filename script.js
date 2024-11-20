0049c93463ce7953d3294a918e19d65f16672e90
pedro-rennil <phrss@cesar.school>
2024-11-20 17:00:17 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburguer-icon")
     menu.classList.toggle("open") 
     icon.classList.toggle("open") 
 }
